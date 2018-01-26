970ea0d439028155b1da73c9ec33a1711571eb96
Peng Zhang <pengzhang@Pengs-MacBook-Pro.local>
2018-01-26 15:09:35 +0100

    change like icon, delete slide function

--- pages/movieDetails/movieDetails.js
+++ pages/movieDetails/movieDetails.js
@@ -49,24 +49,6 @@ Page({
       })
     })
 
-    // if (released == 0) {
-    //   movies = app.globalData.releasedMovies
-    // } else {
-    //   movies = app.globalData.willReleaseMovies
-    // }
-
-    // for (var i = 0; i < movies.length; i++) {
-    //   if (id == movies[i].id) {
-    //     wx.setNavigationBarTitle({
-    //       title: movies[i].title,
-    //     })
-    //     this.setData({
-    //       movie: movies[i],
-    //       actors: movies[i].actors
-    //     })
-    //   }
-    // }
-
     let { tabs } = this.data;
     var res = wx.getSystemInfoSync()
     this.windowWidth = res.windowWidth;
@@ -138,82 +120,19 @@ Page({
     })
   },
 
-  handlerStart(e) {
-    let { clientX, clientY } = e.touches[0];
-    this.startX = clientX;
-    this.tapStartX = clientX;
-    this.tapStartY = clientY;
-    this.data.stv.tStart = true;
-    this.tapStartTime = e.timeStamp;
-    this.setData({ stv: this.data.stv })
-  },
-
-  handlerMove(e) {
-    let { clientX, clientY } = e.touches[0];
-    let { stv } = this.data;
-    let offsetX = this.startX - clientX;
-    this.startX = clientX;
-    stv.offset += offsetX;
-    if (stv.offset <= 0) {
-      stv.offset = 0;
-    } else if (stv.offset >= stv.windowWidth * (this.tabsCount - 1)) {
-      stv.offset = stv.windowWidth * (this.tabsCount - 1);
-    }
-    this.setData({ stv: stv });
-  },
-
-  handlerCancel(e) {
-
-  },
-
-  handlerEnd(e) {
-    let { clientX, clientY } = e.changedTouches[0];
-    let endTime = e.timeStamp;
-    let { tabs, stv, activeTab } = this.data;
-    let { offset, windowWidth } = stv;
-    //快速滑动
-    if (endTime - this.tapStartTime <= 300) {
-      //向左
-      if (Math.abs(this.tapStartY - clientY) < 50) {
-        if (this.tapStartX - clientX > 5) {
-          if (activeTab < this.tabsCount - 1) {
-            this.setData({ activeTab: ++activeTab })
-          }
-        } else {
-          if (activeTab > 0) {
-            this.setData({ activeTab: --activeTab })
-          }
-        }
-        stv.offset = stv.windowWidth * activeTab;
-      } else {
-        //快速滑动 但是Y距离大于50 所以用户是左右滚动
-        let page = Math.round(offset / windowWidth);
-        if (activeTab != page) {
-          this.setData({ activeTab: page })
-        }
-        stv.offset = stv.windowWidth * page;
-      }
-    } else {
-      let page = Math.round(offset / windowWidth);
-      if (activeTab != page) {
-        this.setData({ activeTab: page })
-      }
-      stv.offset = stv.windowWidth * page;
-    }
-    stv.tStart = false;
-    this.setData({ stv: this.data.stv })
-  },
-
   updateSelectedPage(page) {
     let { tabs, stv, activeTab } = this.data;
     activeTab = page;
     this.setData({ activeTab: activeTab })
     stv.offset = stv.windowWidth * activeTab;
     this.setData({ stv: this.data.stv })
-    console.log("page: " + this.data.activeTab)
   },
 
   handlerTabTap(e) {
     this.updateSelectedPage(e.currentTarget.dataset.index);
+  },
+
+  onLikeClicked: function(e) {
+    console.log("onLikeClicked")
   }
 })
