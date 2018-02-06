1d89f75d241df7aa597e416514a200f4f3a61f62
Peng <zhangpeng_com_cn@gmx.de>
2018-02-06 01:01:14 +0100

    login

--- app.js
+++ app.js
@@ -12,25 +12,56 @@ App({
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+    // // 获取用户信息
+    // wx.getSetting({
+    //   success: res => {
+    //     if (res.authSetting['scope.userInfo']) {
+    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+    //       wx.getUserInfo({
+    //         success: res => {
+    //           // 可以将 res 发送给后台解码出 unionId
+    //           this.globalData.userInfo = res.userInfo
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
+    //           // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //           // 所以此处加入 callback 以防止这种情况
+    //           if (this.userInfoReadyCallback) {
+    //             this.userInfoReadyCallback(res)
+    //           }
+    //         }
+    //       })
+    //     }
+    //   }
+    // })
+
+    wx.login({
+      success: function (loginResult) {
+        wx.getUserInfo({
+          success: function (userResult) {
+            console.log("login: ", userResult.userInfo)
+            console.log("code: ", loginResult.code)
+            console.log("encryptedData: ", userResult.encryptedData)
+            console.log("iv: ", userResult.iv)
+            // callback(null, {
+            //   code: loginResult.code,
+            //   encryptedData: userResult.encryptedData,
+            //   iv: userResult.iv,
+            //   userInfo: userResult.userInfo,
+            // })
+          },
+
+          fail: function (userError) {
+            // var error = new LoginError(constants.ERR_WX_GET_USER_INFO, '获取微信用户信息失败，请检查网络状态')
+            // error.detail = userError
+            // callback(error, null)
+          },
+        });
+      },
+
+      fail: function (loginError) {
+        // var error = new LoginError(constants.ERR_WX_LOGIN_FAILED, '微信登录失败，请检查网络状态')
+        // error.detail = loginError
+        // callback(error, null)
+      },
     })
   },
   globalData: {
