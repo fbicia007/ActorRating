5e7710fb7288e5e1cdc216788dc3dbb7847486d8
Peng <zhangpeng_com_cn@gmx.de>
2018-02-06 23:16:51 +0100

    Add server

---
+++ client/app.js
@@ -0,0 +1,53 @@
+var qcloud = require('./vendor/wafer2-client-sdk/index')
+var config = require('./config')
+
+
+App({
+  onLaunch: function () {
+    qcloud.setLoginUrl(config.service.loginUrl)
+  },
+  globalData: {
+    userInfo: null,
+    basicUrl: "https://xuwang.de/actorrating",
+    statusList: { "0": "coming_soon", "1": "in_theaters" },
+    pageTypelist: { "coming_soon": "即将上映", "in_theaters": "正在热映" }
+  },
+  getFilmInfo: function (pageType, start, count, cb) {
+    var that = this
+
+    wx.request({
+      url: that.globalData.basicUrl + "/" + pageType + '?start=' + start + '&count=' + count,
+      header: {
+        "Content-Type": "json",
+      },
+      success: function (res) {
+        cb(res)
+      }
+    })
+  },
+  getFilmDetail: function (pageType, id, cb) {
+    var that = this
+    console.log("url: ", that.globalData.basicUrl + "/" + pageType + '?id=' + id)
+    wx.request({
+      url: that.globalData.basicUrl + "/" + pageType + '?id=' + id,
+      header: {
+        "Content-Type": "json",
+      },
+      success: function (res) {
+        cb(res)
+      }
+    })
+  },
+  getSearchRequest: function (text, cb) {
+    var that = this
+    wx.request({
+      url: that.globalData.basicUrl + "/search?srch_text=" + text,
+      header: {
+        "Content-Type": "json",
+      },
+      success: function (res) {
+        cb(res)
+      }
+    })
+  }
+})
---
+++ client/pages/home/home.js
@@ -0,0 +1,163 @@
+var qcloud = require('../../vendor/wafer2-client-sdk/index')
+var config = require('../../config')
+var util = require('../../utils/util.js')
+
+var app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    films: [{}, {}],
+    userInfo: {},
+    logged: false,
+    takeSession: false,
+    requestResult: ''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.setNavigationBarTitle({
+      title: '演员评分',
+    })
+
+    if (this.data.logged) return
+
+    util.showBusy('正在登录')
+    var that = this
+
+    // 调用登录接口
+    qcloud.login({
+      success(result) {
+        console.log("login: ", result)
+        if (result) {
+          util.showSuccess('登录成功')
+          that.setData({
+            userInfo: result,
+            logged: true
+          })
+        } else {
+          // 如果不是首次登录，不会返回用户信息，请求用户信息接口获取
+          qcloud.request({
+            url: config.service.requestUrl,
+            login: true,
+            success(result) {
+              util.showSuccess('登录成功')
+              that.setData({
+                userInfo: result.data.data,
+                logged: true
+              })
+            },
+
+            fail(error) {
+              util.showModel('请求失败', error)
+              console.log('request fail', error)
+            }
+          })
+        }
+      },
+
+      fail(error) {
+        util.showModel('登录失败', error)
+        console.log('登录失败', error)
+      }
+    })
+
+    // wx.showLoading({
+    //   title: '全力加载中...',
+    // })
+
+    // var that = this
+    // var statusList = Object.values(app.globalData.statusList).reverse()
+    // var titlelist = app.globalData.pageTypelist
+    // for (let i = 0; i < statusList.length; i++) {
+    //   console.log("status: ", statusList[i])
+    //   app.getFilmInfo(statusList[i], 0, 7, function (res) {
+    //     wx.hideLoading()
+    //     var data = res.data
+
+    //     that.data.films[i] = { title: titlelist[statusList[i]], data: data, status: statusList[i] }
+    //     that.setData({
+    //       films: that.data.films
+    //     })
+    //     console.log("films: ", that.data.films)
+    //   })
+    // }
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  onDetailClicked: function (e) {
+    var data = e.currentTarget.dataset
+    console.log("click id: ", data.id, " stauts: ", data.status)
+    wx.navigateTo({
+      url: '../movieDetails/movieDetails?id=' + data.id + "&status=" + data.status,
+    })
+  },
+
+  onMoreClicked: function (e) {
+    var temp = e.currentTarget.dataset;
+    console.log("tmp: ", temp.type)
+    wx.navigateTo({
+      url: '../list/list?type=' + temp.type
+    })
+  },
+
+  onSearchClicked: function (e) {
+    wx.navigateTo({
+      url: '../search/search'
+    })
+  },
+
+})
